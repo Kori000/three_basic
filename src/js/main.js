0f02636ac5fa0eddacedab76b71d5a1ee92774fa
Kori <kexin@korix.top>
2022-11-10 00:14:49 +0800

    增加轨道控制器 added orbit controller

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 // 引入 three.js
 import * as THREE from 'three';
 
+// 引入 轨道控制器
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 // 创建场景
 const scene = new THREE.Scene()
 
@@ -41,6 +44,20 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 // 使用渲染器, 通过相机将场景渲染进来
-renderer.render(scene, camera)
+// renderer.render(scene, camera)
 
+// 创建轨道控制器
+// 传入 相机 和 用于事件监听的 HTML 元素 (canvas)
+const controls = new OrbitControls(camera, renderer.domElement)
 
+// window.requestAnimationFrame()
+// 告诉浏览器——你希望执行一个动画，并且要求浏览器在下次重绘之前调用指定的回调函数更新动画。
+// 该方法需要传入一个回调函数作为参数，该回调函数会在浏览器下一次重绘之前执行
+
+function render () {
+  renderer.render(scene, camera)
+  // 浏览器每渲染一帧都会调用 render 函数
+  requestAnimationFrame(render)
+}
+
+render()
